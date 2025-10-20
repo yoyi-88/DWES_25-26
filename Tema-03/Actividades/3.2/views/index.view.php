6f5dc663ff0a63094395a87e56c3f5f4411d97e2
yoyi-88 <ygomben2510@g.educaand.es>
2025-10-20 18:58:01 +0200

    Subida actividad 3.2

---
+++ Tema-03/Actividades/3.2/views/index.view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 3.2 - Tabla de Libros</title>
+
+    <!-- css bootstrap5.3.8 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- Bootstrap icons 1.13.1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <!-- capa principal -->
+    <div class="container mt-3">
+
+        <!-- cabecera del documento -->
+        <header class="pb-1 mb-4 border-bottom">
+            <span class="fs-1">Actividad 3.2 - Tabla de Libros</span>
+            <p>Gestión tabla libros</p>
+        </header>
+
+        <!-- contenido principal -->
+        <main>
+            <h4 class="border-bottom">Tabla Libros</h4>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Autor</th>
+                        <th scope="col">Género</th>
+                        <th scope="col">Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php for ($i = 0; $i < count($libros); $i++): ?>
+                        <tr>
+                            <th scope="row"><?= $i ?></th>
+                            <td><?= htmlspecialchars($libros[$i]['titulo']) ?></td>
+                            <td><?= htmlspecialchars($libros[$i]['autor']) ?></td>
+                            <td><?= htmlspecialchars($libros[$i]['genero']) ?></td>
+                            <td><?= htmlspecialchars($libros[$i]['precio']) ?></td>
+                        </tr>
+
+                    <?php endfor; ?>
+                   
+
+                </tbody>
+            </table>
+
+
+        </main>
+
+        <!-- pie de página -->
+        <footer class="footer mt-auto py-3 fixed-bottom bg-light">
+            <div class="container">
+                <span class="text-muted">&copy; 2025
+                    Yoël Gómez Benítez - DWES - 2º DAW - Curso 25/26</span>
+            </div>
+        </footer>
+    </div>
+    <!-- javascript bootstrap5.3.8 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
